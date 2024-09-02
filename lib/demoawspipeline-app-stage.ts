4f5bfc6583f2a5f4f6b5f97f78b023fe15dc8246
Sven van Collenburg <sven@hienfeld.nl>
2024-09-02 15:18:55 +0200

    added new pipeline stage

---
+++ lib/demoawspipeline-app-stage.ts
@@ -0,0 +1,13 @@
+import * as cdk from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+import { lambdaStack } from './lambda-stacks';
+
+export class PipelineAppStage extends cdk.Stage {
+
+    constructor (scope: Construct, id: string, props?: cdk.StageProps) {
+        super(scope, id, props);
+
+        const demolambdastack = new lambdaStack(this, 'LambdaStack');
+    }
+
+}
